de970b1f597b4265d553b29b003d664e3944ec35
Matt Wagner <Matt@RedLeg.dev>
2021-04-09 19:37:30 -0400

    Add Git module

---
+++ Git.psm1
@@ -0,0 +1,44 @@
+function Execute-GitAmend
+{
+    git commit --amend --no-edit
+}
+Set-Alias amend Execute-GitAmend
+
+function Execute-GitPullRebasePrune
+{
+    git stash
+    git fetch --prune
+    git rebase origin/main
+    git stash pop
+}
+Set-Alias gitp Execute-GitPullRebasePrune
+
+function Update-AllRepositories
+{
+    # Cycle through all of the top level folders and pull the latest
+
+    foreach($dir in (Get-ChildItem -Directory))
+    {
+        Push-Location $dir
+
+        if(Test-Path .git)
+        {
+            Execute-GitPullRebasePrune
+        }
+
+        Pop-Location
+    }
+}
+
+if(!(Test-Path 'C:\tools\poshgit\dahlbyk-posh-git*'))
+{
+    choco install poshgit
+}
+
+Import-Module (Resolve-Path 'C:\tools\poshgit\dahlbyk-posh-git*\src\posh-git.psd1')
+
+$env:GIT_SSH = "C:\WINDOWS\System32\OpenSSH\ssh.exe"
+
+Start-SshAgent -Silent
+
+Export-ModuleMember -Alias * -Function *
