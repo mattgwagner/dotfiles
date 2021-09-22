08e78ad215c0c06dca7b2c19e9f62b2cc740a8c1
Matt Wagner <Matt@RedLeg.dev>
2021-09-22 09:31:51 -0400

    Create code folder

--- Common.psm1
+++ Common.psm1
@@ -2,4 +2,9 @@ $Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $Code = Join-Path $($env:USERPROFILE) "Code"
 
+if(!(test-path $Code))
+{
+    New-Item -Path $Code -ItemType Directory
+}
+
 Export-ModuleMember -variable * -Function *
