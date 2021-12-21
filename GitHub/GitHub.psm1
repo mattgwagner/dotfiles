6a4f243d8211b9693e03bca633142b193285c46f
Matt Wagner <Matt@RedLeg.dev>
2021-12-21 15:38:37 -0500

    Didn't work

--- GitHub/GitHub.psm1
+++ GitHub/GitHub.psm1
@@ -4,15 +4,4 @@ Import-Module $Here\AutoComplete.psm1
 
 $env:Path = "$($env:Path);$Here;"
 
-function GetAllReposAsCsv($Organization)
-{
-    $Repos = (& Get-AllRepositories.ps1 -Organization $Organization)
-
-    $Content = ($Repos | Select-Object -Property name, url, archived)
-
-    $Csv = (ConvertTo-Csv $Content)
-
-    return $Csv
-}
-
 Export-ModuleMember -Alias * -Function *
