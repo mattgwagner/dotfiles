68431086f57a189399b905d398ede5b145b0143e
Matt Wagner <Matt@RedLeg.dev>
2021-12-21 15:26:45 -0500

    Try to get multiple pages

--- GitHub/Get-AllRepositories.ps1
+++ GitHub/Get-AllRepositories.ps1
@@ -2,6 +2,23 @@ param (
     $Organization
 )
 
-$Repos = gh api "https://api.github.com/orgs/$Organization/repos?per_page=100"
+$Repos = @();
 
-return ConvertFrom-Json $Repos
+$Retrieved = 0;
+
+do
+{
+    $Results = gh api "https://api.github.com/orgs/$Organization/repos?per_page=100"
+
+    $FromJson = ConvertFrom-Json $Results
+
+    foreach($Repo in $FromJson)
+    {
+        $Repos.Add($Repo)
+    }
+
+    $Retrieved = $FromJson.Count
+}
+while($Retrieved -gt 0);
+
+return $Repos
