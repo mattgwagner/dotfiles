9be0791aa090d9b33f25777055adccbd25b6bd2d
Matt Wagner <Matt@RedLeg.dev>
2021-12-21 15:51:31 -0500

    Csv again

--- GitHub/GitHub.psm1
+++ GitHub/GitHub.psm1
@@ -4,4 +4,15 @@ Import-Module $Here\AutoComplete.psm1
 
 $env:Path = "$($env:Path);$Here;"
 
+function ExportRepositoriesForOrganization($Organization)
+{
+    $Repos = (& $Here\Get-AllRepositories.ps1 -Organization $Organization)
+
+    $Selected = ($Repos | Select-Object -Property name, git_url, archived)
+
+    $Csv = (ConvertTo-CSV $Selected)
+
+    Write-Content $Csv -Path "$Organization.csv"
+}
+
 Export-ModuleMember -Alias * -Function *
