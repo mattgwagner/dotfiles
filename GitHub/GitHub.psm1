72529512f0ad03cc04ce9b835ce8b401ee5d1d43
Matt Wagner <Matt@RedLeg.dev>
2021-12-21 15:36:47 -0500

    Csv-ify

--- GitHub/GitHub.psm1
+++ GitHub/GitHub.psm1
@@ -4,4 +4,15 @@ Import-Module $Here\AutoComplete.psm1
 
 $env:Path = "$($env:Path);$Here;"
 
+function GetAllReposAsCsv($Organization)
+{
+    $Repos = (& Get-AllRepositories.ps1 -Organization $Organization)
+
+    $Content = $Repos | Select-Object -Property name, url, archived
+
+    $Csv = ConvertTo-Csv $Content
+
+    return $Csv
+}
+
 Export-ModuleMember -Alias * -Function *
