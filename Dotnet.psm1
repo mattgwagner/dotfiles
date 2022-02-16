1356d8a17a843dd250ca3ee5ea27470851e65926
Matt Wagner <Matt@RedLeg.dev>
2022-02-15 22:03:16 -0500

    Update Dotnet.psm1

--- Dotnet.psm1
+++ Dotnet.psm1
@@ -1,3 +1,9 @@
+## Yes, I know these are two distinct different versions
+
+$Vs2019 = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+
+$Vs2022 = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+
 
 function Check-DotnetUpgrade($Project)
 {
@@ -11,4 +17,4 @@ function Ensure-DotnetUpgradeAssistant
     dotnet tool install -g upgrade-assistant --ignore-failed-sources
 }
 
-Export-ModuleMember -Alias * -Function *
+Export-ModuleMember -Alias * -Function * -Variable
