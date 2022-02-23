9bede8a661f768a23a7794b42b1bfc0e8e12185a
mattgwagner <Matt@RedLeg.dev>
2022-02-23 09:50:55 -0500

    Tweak builds

--- Dotnet/Dotnet.psm1
+++ Dotnet/Dotnet.psm1
@@ -2,13 +2,15 @@
 
 ## This should probably be refactored to use vswhere.exe instead
 
+$Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 $Vs2019 = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
 
 $Vs2022 = "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
 
 $Ssms = "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"
 
-$MsBuild = ""
+$MsBuild = (& "$Here\Get-Msbuild.ps1")
 
 function Check-DotnetUpgrade($Project)
 {
