4940b8bbf115fceaf53057e134fb79fe503bfacf
Matt Wagner <Matt@RedLeg.dev>
2022-08-05 15:12:06 -0400

    Ohmyposh macos

--- Ensure-OhMyPosh.ps1
+++ Ensure-OhMyPosh.ps1
@@ -5,11 +5,16 @@ if( ! (Get-Command "oh-my-posh.exe" -ErrorAction SilentlyContinue))
     if($IsWindows)
     {
         Set-ExecutionPolicy Bypass -Scope Process -Force;
+
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+
+        & RefreshEnv.cmd
     }
 
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
-
-    & RefreshEnv.cmd
+    if($IsMacOs)
+    {
+        brew install jandedobbeleer/oh-my-posh/oh-my-posh
+    }
 }
 
 oh-my-posh init pwsh --config $Here\oh-my-posh.json | Invoke-Expression
