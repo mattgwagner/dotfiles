031f8f8feb8385b4e2e9f4ef7dbb18ec2d943670
Matt Wagner <Matt@RedLeg.dev>
2022-08-05 15:06:57 -0400

    More windows wrapping for exec policy

--- Ensure-OhMyPosh.ps1
+++ Ensure-OhMyPosh.ps1
@@ -2,7 +2,12 @@ $Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 if( ! (Get-Command "oh-my-posh.exe" -ErrorAction SilentlyContinue))
 {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+    if($IsWindows)
+    {
+        Set-ExecutionPolicy Bypass -Scope Process -Force;
+    }
+
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
     & RefreshEnv.cmd
 }
